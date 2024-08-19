cdec4288b16425096ba78064fa4fcf0599916f78
jjorgeneto90 <jjorgeneto90@gmail.com>
2024-08-19 10:25:52 -0300

    resolução do exercicio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone ip = new Iphone("123456", "3310","123123", 8);
+Nokia nk = new Nokia("444444", "15 pro", "321321", 16);
+
+
+ip.InstalarAplicativo("whatsapp");
+ip.ReceberLigacao();
+ip.Ligar();
+
+nk.Ligar();
+nk.ReceberLigacao();
+nk.InstalarAplicativo("facebook");
